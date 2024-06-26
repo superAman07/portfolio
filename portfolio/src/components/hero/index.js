97666e50e7434fa8feacd62c255d7c03ba4e73df
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-06-26 16:29:23 +0000

    navbar Completed

--- portfolio/src/components/hero/index.js
+++ portfolio/src/components/hero/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const index = ()=>{
-    return <p>i am hero</p>
+const index = () => {
+    return <div className="h-screen w-full">
+        <div className="mt-28 h-[calc(100vh_-_7rem)] flex ">
+            <div className=" w-1/2 flex flex-col justify-center pl-8">
+                <h1 className="font-bold text-4xl">Hello, I'am Aman Vishwakarma</h1>
+                <h1 className="font-bold text-xl">A Passionate B.Tech Final Yaer Student</h1>
+            </div>
+            <div className="w-1/2 flex items-center justify-center">
+                <h1>Image</h1>
+            </div>
+        </div>
+    </div>
 }
 
 export default index;
--- portfolio/src/components/navbar/index.js
+++ portfolio/src/components/navbar/index.js
@@ -7,21 +7,21 @@ const index = () => {
 
             </div> */}
             <nav class="border-gray-200 rounded-3xl absolute top-6 bg-lime-500 bg-opacity-50 backdrop-blur-[2px] w-[60%] px-4">
-                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">   
-                    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <div class="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="https://i.postimg.cc/tRqqkh53/Screenshot-2024-06-26-212823.png" class="rounded-3xl h-12" alt="Aman Logo" />
                         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AMAN VISHWAKARMA</span>
-                    </a>
+                    </div>
                     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">About</a>
+                                <a href="#about" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" >About</a>
                             </li>
                             <li>
-                                <a href="#  skills" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Skills</a>
+                                <a href="#skills" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Skills</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</a>
+                                <a href="#projects" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</a>
                             </li>
                         </ul>
                     </div>
