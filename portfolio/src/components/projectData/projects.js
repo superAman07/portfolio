3777b96603cbee80d760ee76b2c446c5474e5ff6
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-07-03 20:06:25 +0000

    started project section

---
+++ portfolio/src/components/projectData/projects.js
@@ -0,0 +1,19 @@
+
+const projects = [
+    {
+      id: 1,
+      title: "Project 1",
+      imageUrl: "../images/barknbond.png",
+      description: "I created a unique dating platform designed exclusively for dogs using HTML, CSS, and Bootstrap. This project combines my passion for web development with a playful twist, offering dogs a chance to find their pawfect match online. The website features a modern design, user-friendly interface, and responsive layout, ensuring an enjoyable experience for both canine users and their owners",
+      previewUrl: "https://barknbond.netlify.app/", // Replace with actual preview link
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      imageUrl: "https://via.placeholder.com/400x300",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      previewUrl: "#", // Replace with actual preview link
+    },
+  ];
+  export default projects;
+  
