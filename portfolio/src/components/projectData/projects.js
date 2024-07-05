67b3c5d5030834374ab200ac9bcdf6e11b82768d
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-07-05 19:52:48 +0000

    cleared project part

--- portfolio/src/components/projectData/projects.js
+++ portfolio/src/components/projectData/projects.js
@@ -1,19 +1,36 @@
 import barknbond from "./images/barknbond.png";
+import investmentCalc from "./images/investmentCalc.png";
+import wotwebsite from "./images/wotwebsite.png"; 
+import filterSystem from "./images/filterSystem.png"; 
 
 const projects = [
     {
       id: 1,
+      title: "Filter System",
+      imageUrl: filterSystem,
+      description: "I developed an advanced expense tracker using React, featuring a robust filter system for organizing expenses by date (DD/MM/YYYY). Inspired by platforms like Amazon and Flipkart, users can view and filter expenses based on purchase dates, providing insights into spending habits over time. Additionally, the interface includes a vertical filling bar for visualizing expense distribution, ensuring a comprehensive financial management tool.",
+      previewUrl: "https://filtersystem.vercel.app/",  
+    },
+    {
+      id: 2,
+      title: "investment Calculator",
+      imageUrl: investmentCalc,
+      description: "I developed an intuitive investment calculator using React, featuring essential fields such as CURRENT SAVINGS ($), YEARLY SAVINGS ($), EXPECTED INTEREST (%, PER YEAR), and INVESTMENT DURATION. This tool empowers users to plan and visualize their financial goals effectively, providing accurate projections based on their input parameters.",
+      previewUrl: "https://investment-calculator-d9yx5u5rc.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "WOT website",
+      imageUrl: wotwebsite,
+      description: "I crafted a dynamic e-commerce platform using HTML, CSS, and JavaScript, offering a seamless user experience with full functionality. The product pages showcase multiple images per product, allowing users to explore different varieties effortlessly using an intuitive slider. This project reflects my commitment to creating engaging web solutions tailored for optimal user interaction and satisfaction, inspired by my passion for enhancing digital experiences.",
+      previewUrl: "https://wotwebsite.vercel.app/",
+    },
+    {
+      id: 4,
       title: "barknbond",
       imageUrl: barknbond,
       description: "I created a unique dating platform designed exclusively for dogs using HTML, CSS, and Bootstrap. This project combines my passion for web development with a playful twist, offering dogs a chance to find their pawfect match online. The website features a modern design, user-friendly interface, and responsive layout, ensuring an enjoyable experience for both canine users and their owners",
       previewUrl: "https://barknbond.netlify.app/",  
-    },
-    {
-      id: 2,
-      title: "Project 2",
-      imageUrl: "",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      previewUrl: "#",
     },
   ];
   export default projects;
--- portfolio/src/components/projects/index.js
+++ portfolio/src/components/projects/index.js
@@ -5,42 +5,29 @@ import Image from "next/image";
 const Project = () => {
     console.log("projects");
     return (
-        <div className="flex flex-col items-center bg-[#202426] p-8">
+        <div className="flex flex-col items-center bg-[#202426] p-8 min-h-screen">
             <h2 className="text-4xl text-[#D9D9D9] font-bold text-center mb-12">Projects</h2>
-            <div className="flex flex-wrap justify-center gap-8">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
                 {projects.map((project, index) => (
-                    <div key={project.id} className="bg-[#252426] rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-full md:w-[45%] lg:w-[30%] flex flex-col md:flex-row min-h-[400px]">
-                        {index % 2 === 0 ? (
-                            <>
-                                <div className="relative group w-full md:w-1/2 h-full">
-                                    <Image height={700} width={900} src={project.imageUrl} alt={project.title} className="rounded-lg object-cover h-full" />
-                                    <a href={project.previewUrl} target="_blank" rel="noopener noreferrer" className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                                        <button className="bg-[#252426] text-[#D9D9D9] py-2 px-4 rounded hover:bg-[#6C733D] transition-transform duration-300 transform hover:translate-y-[-2px]">
-                                            Preview
-                                        </button>
-                                    </a>
-                                </div>
-                                <div className="p-6 text-[#D9D9D9] md:w-1/2 flex flex-col justify-center">
-                                    <h3 className="text-2xl font-bold mb-4 text-[#9DA65D]">{project.title}</h3>
-                                    <p className="text-lg text-[#A6A6A6]">{project.description}</p>
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                <div className="p-6 text-[#D9D9D9] md:w-1/2 flex flex-col justify-center">
-                                    <h3 className="text-2xl font-bold mb-4 text-[#9DA65D]">{project.title}</h3>
-                                    <p className="text-lg text-[#A6A6A6]">{project.description}</p>
-                                </div>
-                                <div className="relative group w-full md:w-1/2 h-full">
-                                    <Image height={400} width={600} src={project.imageUrl} alt={project.title} className="rounded-lg object-cover h-full" />
-                                    <a href={project.previewUrl} target="_blank" rel="noopener noreferrer" className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                                        <button className="bg-[#252426] text-[#D9D9D9] py-2 px-4 rounded hover:bg-[#6C733D] transition-transform duration-300 transform hover:translate-y-[-2px]">
-                                            Preview
-                                        </button>
-                                    </a>
-                                </div>
-                            </>
-                        )}
+                    <div key={project.id} className="bg-[#252426] rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
+                        <div className="relative group">
+                            <Image 
+                                src={project.imageUrl} 
+                                alt={project.title} 
+                                className="rounded-t-lg object-cover w-full h-48 sm:h-56 md:h-64 lg:h-72" 
+                                width={600} 
+                                height={400} 
+                            />
+                            <a href={project.previewUrl} target="_blank" rel="noopener noreferrer" className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                                <button className="bg-[#252426] text-[#D9D9D9] py-2 px-4 rounded hover:bg-[#6C733D] transition-transform duration-300 transform hover:translate-y-[-2px]">
+                                    Preview
+                                </button>
+                            </a>
+                        </div>
+                        <div className="p-4 text-[#D9D9D9]">
+                            <h3 className="text-xl font-bold mb-2 text-[#9DA65D]">{project.title}</h3>
+                            <p className="text-sm text-[#A6A6A6]">{project.description}</p>
+                        </div>
                     </div>
                 ))}
             </div>
