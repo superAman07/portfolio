e384a8957b03726ec02a158d4d0c8f4a9c90a72a
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-09-26 08:16:55 +0000

    cleared something in

--- portfolio/src/components/hero/index.js
+++ portfolio/src/components/hero/index.js
@@ -1,22 +1,21 @@
-'use client';
+'use client'
 import React, { useRef } from "react";
 import Navbar from "../navbar/index.js"; 
+// import Typical from 'react-typical'; 
 
 const Hero = () => {
   const contentRef = useRef(null);
 
   const scrollToContent = () => {
-    if (typeof window !== "undefined") { // Check if window is defined
-      const bottomOfViewport = window.innerHeight;
-      window.scrollTo({
-        top: bottomOfViewport,
-        behavior: "smooth",
-      });
-    }
+    const bottomOfViewport = window.innerHeight;
+    window.scrollTo({
+      top: bottomOfViewport,
+      behavior: "smooth",
+    });
   };
 
   return (
-    <div className="relative h-[100dvh] w-full bg-[#252426] text-white flex flex-col justify-center items-center">
+    <div className="relative h-[100dvh]  w-full bg-[#252426] text-white flex flex-col justify-center items-center">
       <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
         <ul className="circles">
           <li className="circle circle-1"></li>
@@ -55,7 +54,17 @@ const Hero = () => {
           />
         </div>
       </div>
+      {/* <div className="text-lg md:text-xl relative top-10 mb-8 px-3 text-[#D9D9D9] min-w-[250px]">
+        <Typical
+          steps={['Welcome to my portfolio!!!', 1500, 'Discover my projects-->', 1500, 'Learn more about me...!', 1500]}
+          loop={Infinity}
+          wrapper="p"
+          className="font-smooth"
+        />
+      </div> */}
 
+
+        
       <div className="absolute bottom-0 left-0 w-full text-center p-2 bg-opacity-75 z-10">
         <p
           className="text-[#D9D9D9] text-sm md:text-base animate-bounce cursor-pointer"
@@ -64,7 +73,7 @@ const Hero = () => {
           Scroll down to see more
         </p>
       </div>
-
+ 
       <style jsx>{`
         .circle {
           position: absolute;
@@ -165,12 +174,27 @@ const Hero = () => {
           }
         }
 
+        /* Background Animation */
+        @keyframes gradientAnimation {
+          0% {
+            background-color: #737373;
+          }
+          100% {
+            background-color: #252426;
+          }
+        }
+
+        .bg-animate {
+          animation: gradientAnimation 10s linear infinite alternate;
+        }
+          // after image typing content
         .font-smooth {
           -webkit-font-smoothing: antialiased;  
         }
         .z-10 {
           z-index: 10;
         }
+
       `}</style>
     </div>
   );
