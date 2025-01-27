d3ad1cd7bdecec9c70082871446dac27a4b936af
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2025-01-27 12:28:24 +0000

    makeing it more lag free

--- portfolio/src/components/projectData/projects.jsx
+++ portfolio/src/components/projectData/projects.jsx
@@ -1,19 +1,19 @@
 'use client'
-import barknbond from "./images/barknbond.png";
-import investmentCalc from "./images/investmentCalc.png"; 
-import todo from "./images/todo.png"; 
-import filterSystem from "./images/filterSystem.png"; 
-import portfolio from "./images/portfolio.png"; 
-import mediumBlog from "./images/medium-blog.png";
-import paytmClone from "./images/paytmClone.png";
-import weatherapp from "./images/weather.png";
-import webweaver from "./images/webweaver.png";
+// import barknbond from "/images/barknbond.png";
+// import investmentCalc from "/images/investmentCalc.png"; 
+// import todo from "/images/todo.png"; 
+// import filterSystem from "/images/filterSystem.png"; 
+// import portfolio from "/images/portfolio.png"; 
+// import mediumBlog from "/images/medium-blog.png";
+// import paytmClone from "/images/paytmClone.png";
+// import weatherapp from "/images/weather.png";
+// import webweaver from "/images/webweaver.png";
 
 const projects = [
     {
       id: 0,
       title: "Web Weaver",
-      imageUrl: webweaver,
+      imageUrl:  "/images/webweaver.png",
       description: `WebWeaver: An AI-powered platform for website creation that translates natural language prompts into functional websites with code and Preview section to preview also have a Prompt Searching field to communicate with AI regarding project...........
       <-------------------Technologies Used--------------------->
       Frontend: React.js (TypeScript), Tailwind CSS, Axios, Monaco Editor
@@ -26,7 +26,7 @@ const projects = [
     {
       id: 1,
       title: "PayTM Clone",
-      imageUrl: paytmClone,
+      imageUrl: '/images/paytmClone.png',
       description: "A web application that simulates a money transfer platform similar to Paytm. Users can sign up, manage contacts, and transfer money securely through a user-friendly interface. Features: User Registration, Authentication , Dashboard ,Money Transfer, Search Functionality, Responsive Design......................<---------------------Technologies Used-------------------->:  Frontend: React.js, Tailwind CSS  Backend: Node.js, Express.js, MongoDB Others: CORS, Axios",
       previewUrl: "https://github.com/superAman07/PaytmClone",  
       githubUrl: "https://github.com/superAman07/PaytmClone",  
@@ -34,7 +34,7 @@ const projects = [
     {
       id: 2,
       title: "Portfolio",
-      imageUrl: portfolio,
+      imageUrl: "/images/portfolio.png",
       description: "My portfolio, created using Next.js and Tailwind CSS, showcases a blend of modern frontend technologies. It offers a responsive design, smooth navigation, and detailed project displays with preview options. Powered by Next.js for efficient rendering and Tailwind CSS for sleek styling, it highlights my skills in web development and design innovation.",
       previewUrl: "https://aman-vishwakarma.vercel.app/",  
       githubUrl: "https://github.com/superAman07/portfolio"
@@ -42,7 +42,7 @@ const projects = [
     {
       id: 3,
       title: "Medium Blog",
-      imageUrl: mediumBlog,
+      imageUrl: "/images/medium-blog.png",
       description: "Medium Blog Clone is a responsive web application that replicates the core functionalities of the Medium blogging platform. Users can explore blogs, read articles, and publish their own content seamlessly. The project features a modular architecture, with a shared `common` folder utilized for both frontend and backend code. Built with React for the frontend and Cloudflare for the serverless backend, the application ensures scalability and efficiency. This project provides an intuitive user interface and dynamic functionality, offering a clean and simple experience for exploring and publishing blogs.",
       previewUrl: "https://medium-blog-eosin-tau.vercel.app/",  
       githubUrl: "https://github.com/superAman07/medium-blog",  
@@ -50,7 +50,7 @@ const projects = [
     {
       id: 4,
       title: "Weather App",
-      imageUrl: weatherapp,
+      imageUrl: "/images/weather.png",
       description: "I developed an interactive, real-time weather and AQI checker using React and Tailwind CSS, delivering up-to-the-minute weather details and air quality insights for any city or country. The application seamlessly integrates the OpenWeather API, offering users an intuitive interface to explore comprehensive environmental data, including temperature, humidity, and air quality index levels. This project reflects my dedication to creating responsive, user-centered applications that simplify access to crucial information, aligning with my passion for enhancing digital interactions with functional design.",
       previewUrl: "https://weather-app-beryl-eight-73.vercel.app/",
       githubUrl: "https://github.com/superAman07/weather-app",
@@ -58,7 +58,7 @@ const projects = [
     {
       id: 5,
       title: "Todo",
-      imageUrl: todo,
+      imageUrl:  "/images/todo.png",
       description: "A modern Todo List app built with React and Tailwind CSS, featuring task management with add, edit, delete, and complete functionality. Includes category-based filters for better organization. Fully responsive and showcases clean, reusable code with React hooks. Technologies: React, Tailwind CSS, JavaScript and Features: Task management and Category filters",
       previewUrl: "https://todo-278t.vercel.app/",
       githubUrl: "https://github.com/superAman07/todo"
@@ -66,7 +66,7 @@ const projects = [
     {
       id: 6,
       title: "investment Calculator",
-      imageUrl: investmentCalc,
+      imageUrl: "/images/investmentCalc.png",
       description: "I developed an intuitive investment calculator using React, featuring essential fields such as CURRENT SAVINGS ($), YEARLY SAVINGS ($), EXPECTED INTEREST (%, PER YEAR), and INVESTMENT DURATION. This tool empowers users to plan and visualize their financial goals effectively, providing accurate projections based on their input parameters.",
       previewUrl: "https://investment-calculator-d9yx5u5rc.vercel.app/",
       githubUrl: "https://github.com/superAman07/InvestmentCalculator"
@@ -74,7 +74,7 @@ const projects = [
     {
       id: 7,
       title: "barknbond",
-      imageUrl: barknbond,
+      imageUrl: "/images/barknbond.png",
       description: "I created a unique dating platform designed exclusively for dogs using Bootstrap. This project combines my passion for web development with a playful twist, offering dogs a chance to find their pawfect match online. The website features a modern design, user-friendly interface, and responsive layout, ensuring an enjoyable experience for both canine users and their owners",
       previewUrl: "https://barknbond.netlify.app/", 
       githubUrl: "https://barknbond.netlify.app/" 
@@ -82,7 +82,7 @@ const projects = [
     {
       id: 8,
       title: "Filter System",
-      imageUrl: filterSystem,
+      imageUrl: "/images/filterSystem.png",
       description: "I developed an advanced expense tracker using React, featuring a robust filter system for organizing expenses by date (DD/MM/YYYY). Inspired by platforms like Amazon and Flipkart, users can view and filter expenses based on purchase dates, providing insights into spending habits over time. Additionally, the interface includes a vertical filling bar for visualizing expense distribution, ensuring a comprehensive financial management tool.",
       previewUrl: "https://filtersystem.vercel.app/", 
       githubUrl: "https://github.com/superAman07/Filter-system-using-react" 
