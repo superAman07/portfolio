5ac127a919416a190da3f2ed1f77ed5260c7ae71
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2025-02-07 18:12:17 +0000

    cleared some optimization stuffs

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -2,19 +2,17 @@
 import React  from 'react';
 import dynamic from 'next/dynamic';
 
-// const Hero =  dynamic(() => import('../components/hero'), { ssr: false });
-const Hero = dynamic(() => import("../components/hero"), { 
-  ssr: false, 
-  loading: () => <div className="flex items-center justify-center h-screen">
-  <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-black border-opacity-25">
+const Loader = () => (
+  <div className="flex items-center justify-center h-screen">
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-gray-500 border-opacity-50"></div>
   </div>
-</div>
-});
-const Projects = dynamic(() => import('../components/projects'), { ssr: false });
-const Footer =  dynamic(() => import('../components/footer'), { ssr: false });
-const Dot = dynamic(() => import('../components/Dots'), { ssr: false });
-const Contact = dynamic(() => import('../components/contact/contact'), { ssr: false });
-const Skill =  dynamic(() => import('../components/skill'), { ssr: false });
+);
+const Hero =  dynamic(() => import('../components/hero'), { ssr: false ,loading: Loader });
+const Projects = dynamic(() => import('../components/projects'), { ssr: false,loading: Loader });
+const Footer =  dynamic(() => import('../components/footer'), { ssr: false ,loading: Loader});
+const Dot = dynamic(() => import('../components/Dots'), { ssr: false,loading: Loader });
+const Contact = dynamic(() => import('../components/contact/contact'), { ssr: false ,loading: Loader});
+const Skill =  dynamic(() => import('../components/skill'), { ssr: false ,loading: Loader});
 
   
 export default function Home() {
